2dd72347e96697a252d84b847f2c0bb893920e05
blessing333 <blessing_333@naver.com>
2021-05-04 15:21:29 +0900

    Initial commit

---
+++ src/main/java/com/blessing/lentaldream/LentaldreamApplication.java
@@ -0,0 +1,13 @@
+package com.blessing.lentaldream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LentaldreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LentaldreamApplication.class, args);
+    }
+
+}
