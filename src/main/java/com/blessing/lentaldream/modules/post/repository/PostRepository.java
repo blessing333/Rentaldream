cb467e6f1a16bf3e9c97350a8cf0131ddb821a83
blessing333 <blessing_333@naver.com>
2021-10-03 22:33:28 +0900

    메인화면 수정, 메인 컨트롤러 추가

--- src/main/java/com/blessing/lentaldream/modules/post/repository/PostRepository.java
+++ src/main/java/com/blessing/lentaldream/modules/post/repository/PostRepository.java
@@ -4,5 +4,6 @@ import com.blessing.lentaldream.modules.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Post findByDescription(String description);
+    Post findByDescription(String description); //only test
+
 }
