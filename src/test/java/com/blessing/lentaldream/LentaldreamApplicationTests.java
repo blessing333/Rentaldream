16b266e9579e00c299577386fadee6b6708793fb
blessing333 <blessing_333@naver.com>
2021-10-23 01:00:57 +0900

    zones_kr.csv 삭제
    Signed-off-by: blessing333 <blessing_333@naver.com>

--- src/test/java/com/blessing/lentaldream/LentaldreamApplicationTests.java
+++ src/test/java/com/blessing/lentaldream/LentaldreamApplicationTests.java
@@ -2,11 +2,10 @@ package com.blessing.lentaldream;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class LentaldreamApplicationTests {
+
     @Test
     void contextLoads() {
     }
