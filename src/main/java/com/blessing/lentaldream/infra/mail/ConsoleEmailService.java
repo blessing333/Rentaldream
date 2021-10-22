b6125afc9eba592b8107750e927b6cc932ea9b69
blessing333 <blessing_333@naver.com>
2021-10-22 21:11:51 +0900

    애플리케이션 테스트 수정

--- src/main/java/com/blessing/lentaldream/infra/mail/ConsoleEmailService.java
+++ src/main/java/com/blessing/lentaldream/infra/mail/ConsoleEmailService.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 @Slf4j
-@Profile({"local", "test"})
+@Profile({"local","test"})
 @Component
 public class ConsoleEmailService implements EMailService{
     @Override
