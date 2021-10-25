74ca2bcb9b8bba68a0172dec4641ba1eab5316d7
blessing333 <blessing_333@naver.com>
2021-10-25 20:53:29 +0900

    application test 수정
    Signed-off-by: blessing333 <blessing_333@naver.com>

--- src/test/java/com/blessing/rentaldream/RentaldreamApplicationTests.java
+++ src/test/java/com/blessing/rentaldream/RentaldreamApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = RentaldreamApplication.class)
 @ActiveProfiles("test")
 class RentaldreamApplicationTests {
 
