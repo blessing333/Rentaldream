95c05bb2976dd5ced3130dc388c9aa9c2a647d58
blessing333 <blessing_333@naver.com>
2021-10-25 20:08:07 +0900

    테스트코드수정
    Signed-off-by: blessing333 <blessing_333@naver.com>

--- src/test/java/com/blessing/rentaldream/LentaldreamApplicationTests.java
+++ src/test/java/com/blessing/rentaldream/LentaldreamApplicationTests.java
@@ -2,8 +2,10 @@ package com.blessing.rentaldream;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LentaldreamApplicationTests {
 
     @Test
