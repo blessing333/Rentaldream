b4c002f2dec5c12e0ccf6235740804b2b1932807
blessing333 <blessing_333@naver.com>
2021-10-28 01:32:15 +0900

    배포 설정 완료

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ PROJECT_NAME=Rentaldream
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep Rentaldream | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl Rentaldream | grep jar | awk '{print $1}')
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
 echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
