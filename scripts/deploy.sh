524e718042def37f7647239f96b3c19abd84d555
blessing333 <blessing_333@naver.com>
2021-10-28 01:23:09 +0900

    배포 설정 완료

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,5 +19,6 @@ echo "> JAR Name: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
-nohup java -jar -Dspring.profiles.active=prod
-$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar \
+        -Dspring.profiles.active=prod \
+        $JAR_NAME  > $REPOSITORY/nohup.out 2>&1 &
